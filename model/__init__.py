440e47363242c0bfe80de93ec9a0116f4ca69894
Junyong Park <junyong300@gmai.com>
2022-01-25 16:11:56 +0900

    revert

--- model/__init__.py
+++ model/__init__.py
@@ -1,14 +1,15 @@
 import torch
-from model.backbone.resnet import resnet18, resnet34
+from model.backbone.resnet import resnet18, resnet34, resnet50
 
 def get_backbone(cfg):
     print(cfg['backbone'])
     model = None
     if cfg['backbone'] == "resnet18":
-        model = resnet18()
+        model = resnet18(num_classes=cfg['num_class'])
     elif cfg['backbone'] == "resnet34":
-        model = resnet34()
-
+        model = resnet34(num_classes=cfg['num_class'])
+    elif cfg['backbone'] == "resnet50":
+        model = resnet50(num_classes=cfg['num_class'])
     print("try to get pretrained backbone")
     try:
         model.load_state_dict(torch.load(cfg['backbone_path']),strict=False)
--- utils/dataset.py
+++ utils/dataset.py
@@ -7,7 +7,7 @@ def get_dataloader(cfg):
     trainset, testset = get_dataset(cfg)      
     train_loader   = torch.utils.data.DataLoader(trainset, batch_size= cfg['batch_size'], shuffle=True,  num_workers=cfg['workers'])
     val_loader     = torch.utils.data.DataLoader(testset,  batch_size= cfg['batch_size'], shuffle=False, num_workers=cfg['workers'])
-    test_loader    = torch.utils.data.DataLoader(testset,  batch_size= 1,               shuffle=True,    num_workers=cfg['workers'])
+    test_loader    = torch.utils.data.DataLoader(testset,  batch_size= 1,                 shuffle=True,    num_workers=cfg['workers'])
     return train_loader, val_loader, test_loader, trainset, testset
 
 def get_dataset(cfg):
@@ -23,9 +23,9 @@ def get_dataset(cfg):
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2616)),])
 
         trainset = torchvision.datasets.CIFAR10(
-            root = cfg['path']['dataset_path'], train=True, download=True, transform=transform_train)
+            root = cfg['dataset_path'], train=True, download=True, transform=transform_train)
         testset = torchvision.datasets.CIFAR10(
-            root = cfg['path']['dataset_path'], train=False, download=True, transform=transform_test)
+            root = cfg['dataset_path'], train=False, download=True, transform=transform_test)
 
     elif cfg['dataset']  == "cifar100":
         transform_train = transforms.Compose([
@@ -41,10 +41,10 @@ def get_dataset(cfg):
         ])
 
         trainset = torchvision.datasets.CIFAR100(
-            root = cfg['path']['dataset_path'], train=True, download=True, transform=transform_train)
+            root = cfg['dataset_path'], train=True, download=True, transform=transform_train)
 
         testset = torchvision.datasets.CIFAR100(
-            root = cfg['path']['dataset_path'], train=False, download=True, transform=transform_test)
+            root = cfg['dataset_path'], train=False, download=True, transform=transform_test)
 
     elif cfg['dataset']  == 'mnist':
         transform=transforms.Compose([
@@ -53,8 +53,8 @@ def get_dataset(cfg):
             transforms.Normalize((0.1307,), (0.3081,)),
         ])
 
-        trainset = torchvision.datasets.MNIST(cfg['path']['dataset_path'], train=True, download=True, transform=transform)
-        testset = torchvision.datasets.MNIST(cfg['path']['dataset_path'], train=False, transform=transform)
+        trainset = torchvision.datasets.MNIST(cfg['dataset_path'], train=True, download=True, transform=transform)
+        testset = torchvision.datasets.MNIST(cfg['dataset_path'], train=False, transform=transform)
 
 
     return trainset, testset
