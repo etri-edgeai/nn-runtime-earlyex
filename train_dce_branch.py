ddbdb7819353a6099f076db2daee1eb0e399e404
Junyong Park <junyong300@gmai.com>
2022-01-25 18:55:53 +0900

    add devour model to chew

--- train_dce_branch.py
+++ train_dce_branch.py
@@ -1,37 +1,20 @@
 import yaml
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torchvision import transforms
-from torchvision import datasets
-from tqdm import tqdm
-import numpy as np
-from datetime import datetime
-from metric_ex.utils import config, get_dataloader, get_dataset
-from metric_ex.model.backbone import get_backbone
-from metric_ex.trainer.ce_branch import CEBranchTrainer
-from metric_ex.model import Model
-from metric_ex.model.devour import DevourModel
-import argparse 
-import sys
-from metric_ex.model.backbone import resnet18
-from metric_ex.model.backbone.efficientnet import EfficientNet
-from torchvision.models.mobilenetv2 import mobilenet_v2
-from torchvision.models.vgg import vgg16_bn
-
-
+import argparse
+from early_ex.utils import *
+from early_ex.model.devour import DevourModel
+from early_ex.model.backbone import get_backbone
 
 def main():
-    print("Branch Trainer v0.5")
+    print("Devour & Branch Trainer v0.9")
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, default = "./configs/train.yml")
+    parser.add_argument(
+        '--config', type=str, 
+        default = "./configs/base.yml")
     args = parser.parse_args()
     cfg = config(args.config)
-    backbone = resnet18(cfg)
+    backbone = get_backbone(cfg)
 
-    model = DevourModel(backbone, cfg)
-
-    model.hunt(keyword= "Sequential", out="Sequential")
-
+    model = DevourModel(cfg, N=cfg['num_exits'])
+    model.devour(backbone, cfg['backbone'])
 if __name__ == "__main__":
     main()
