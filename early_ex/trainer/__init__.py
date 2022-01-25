4953514c1b0e7d7af40799cd2d3e904f5bbd9011
Junyong Park <junyong300@gmai.com>
2022-01-25 16:32:38 +0900

    add trainer.py

---
+++ early_ex/trainer/__init__.py
@@ -0,0 +1,25 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import yaml
+import os
+
+
+class Trainer():
+    def __init__(self, model, cfg):
+        self.cfg            = cfg
+        self.batch_size     = cfg['batch_size']
+        self.best           = 0
+        self.device         = torch.device(cfg['device'])
+
+        # self.train_loader, self.val_loader, self.test_loader, self.trainset, self.testset = get_dataloader(cfg)
+        self.model = model
+        
+        print('criterion: Cross entropy loss')
+        self.criterion = nn.CrossEntropyLoss()
+        print('optimizer: Adam, lr: ',cfg['lr'])
+        self.optimizer = torch.optim.Adam(self.model.backbone.parameters(), lr = self.cfg['lr'])
+        print('scheduler = multistep LR')
+        self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=self.cfg['backbone_training']['milestone'], gamma=0.5)
