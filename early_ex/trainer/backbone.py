3203863d5dba3880e9d4fbb090a3f777f433ac66
Junyong Park <junyong300@gmai.com>
2022-01-25 16:33:04 +0900

    add backbone python file to load backbone models

---
+++ early_ex/trainer/backbone.py
@@ -0,0 +1,77 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import yaml
+import os
+from tqdm import tqdm
+from . import Trainer
+
+class BackboneTrainer(Trainer):
+
+    def __init__(self, model, cfg) -> None:
+        super().__init__(model, cfg)
+
+    def backbone_train(self, epoch):
+        print("Trainer backbone model...")
+        self.model.backbone.to(self.device)
+        print("Epoch " + str(epoch) + ':')
+        self.model.backbone.train() 
+        tbar = tqdm(self.train_loader)
+        train_loss = 0.0
+        for i, data in enumerate(tbar): 
+            input = data[0].to(self.device)
+            label = data[1].to(self.device)
+
+            self.optimizer.zero_grad()
+            pred = self.model.forward(input)
+            loss = self.criterion(pred, label)
+            _, pred = torch.max(pred, 1)
+            loss.backward()
+            self.optimizer.step()
+
+    def backbone_valid(self, epoch=1):
+        tbar = tqdm(self.val_loader, desc='\r')
+        self.model.backbone.eval()
+        val_loss = 0.0
+        correct = 0
+        total = 0
+        pred_correct = 0
+
+        with torch.no_grad():
+            for i, data in enumerate(tbar):
+                input = data[0].to(self.device)
+                label = data[1].to(self.device)
+                total += label.size(0)
+                pred = self.model.forward(input)
+                loss = self.criterion(pred, label)
+                _, pred = torch.max(pred, 1)
+                val_loss += loss.item()
+                correct += pred.eq(label).sum().item()
+                tbar.set_description('Val loss: %.6f' % (val_loss / ((i + 1)*self.batch_size)))
+
+            acc = 100.*correct/total
+            print("epoch: ",epoch, " accuracy: ",acc)
+
+
+    def open_time_test(self):
+        self.model.backbone.eval()
+        for i in range(self.model.ex_num):
+            m = self.model.exactly[i]
+            m.gate = True
+            m.knn_gate=False
+            m.cross= True
+
+        tbar = tqdm(self.test_loader)
+        acc = 0 
+        total = 0
+        for (i, data) in enumerate(tbar):
+            input = data[0].to(self.device)
+            label = data[1]
+            total += input.shape[0]
+            pred = self.model.forward(input)
+            _ , pred = torch.max(pred, 1)
+            acc += pred.eq(label).sum().item()
+            tbar.set_description("total: {}, correct:{}".format(total, acc))
+        print(print("accuracy: ", acc/ total))
