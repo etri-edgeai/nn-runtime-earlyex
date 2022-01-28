d590b8a2b712331c74fb8543d543a4a10abbd0b3
Junyong Park <junyong300@gmai.com>
2022-01-28 18:35:58 +0900

    fix errors

--- early_ex/trainer/__init__.py
+++ early_ex/trainer/__init__.py
@@ -14,12 +14,11 @@ class Trainer():
         self.best           = 0
         self.device         = torch.device(cfg['device'])
 
-        # self.train_loader, self.val_loader, self.test_loader, self.trainset, self.testset = get_dataloader(cfg)
         self.model = model
         
-        print('criterion: Cross entropy loss')
-        self.criterion = nn.CrossEntropyLoss()
-        print('optimizer: Adam, lr: ',cfg['lr'])
-        self.optimizer = torch.optim.Adam(self.model.backbone.parameters(), lr = self.cfg['lr'])
-        print('scheduler = multistep LR')
-        self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=self.cfg['backbone_training']['milestone'], gamma=0.5)
+        # print('criterion: Cross entropy loss')
+        # self.criterion = nn.CrossEntropyLoss()
+        # print('optimizer: Adam, lr: ',cfg['lr'])
+        # self.optimizer = torch.optim.Adam(self.model.backbone.parameters(), lr = self.cfg['lr'])
+        # print('scheduler = multistep LR')
+        # self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=self.cfg['backbone_training']['milestone'], gamma=0.5)
