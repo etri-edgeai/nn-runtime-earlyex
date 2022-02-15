d73026644fc95eddc6d6e432994bc9880c1c41c3
Junyong Park <junyong300@gmai.com>
2022-02-15 19:00:45 +0900

    comment out

--- train_me_branch.py
+++ train_me_branch.py
@@ -1,83 +1,83 @@
-import yaml
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torchvision import transforms
-from torchvision import datasets
-from tqdm import tqdm
-import numpy as np
-from datetime import datetime
-from metric_ex.utils import config, get_dataloader, get_dataset
-from metric_ex.model.backbone import get_backbone
-from metric_ex.trainer.me_branch import MEBranchTrainer
-from metric_ex.model import Model
-import argparse 
-import sys
+# import yaml
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# from torchvision import transforms
+# from torchvision import datasets
+# from tqdm import tqdm
+# import numpy as np
+# from datetime import datetime
+# from metric_ex.utils import config, get_dataloader, get_dataset
+# from metric_ex.model.backbone import get_backbone
+# from metric_ex.trainer.me_branch import MEBranchTrainer
+# from metric_ex.model import Model
+# import argparse 
+# import sys
 
-def main():
-    print("Branch Trainer v0.5")
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, default = "./configs/train.yml")
-    args = parser.parse_args()
-    cfg = config(args.config)
-    backbone = get_backbone(cfg)
-    try:
-        print("loading pre-trained backbone",cfg['backbone_path'])
-        backbone.load_state_dict(
-            torch.load(cfg['backbone_path']),strict=False)
-    except RuntimeError as e:
-        print(e)
-    except FileNotFoundError as e:
-        print(e)
-        print("Backbone file not found! Maybe try training it first?")
-    model = Model(backbone, cfg)
-    trainer = MEBranchTrainer(model, cfg)
-    trainer.trainset, trainer.testset = get_dataset(cfg)
+# def main():
+#     print("Branch Trainer v0.5")
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--config', type=str, default = "./configs/train.yml")
+#     args = parser.parse_args()
+#     cfg = config(args.config)
+#     backbone = get_backbone(cfg)
+#     try:
+#         print("loading pre-trained backbone",cfg['backbone_path'])
+#         backbone.load_state_dict(
+#             torch.load(cfg['backbone_path']),strict=False)
+#     except RuntimeError as e:
+#         print(e)
+#     except FileNotFoundError as e:
+#         print(e)
+#         print("Backbone file not found! Maybe try training it first?")
+#     model = Model(backbone, cfg)
+#     trainer = MEBranchTrainer(model, cfg)
+#     trainer.trainset, trainer.testset = get_dataset(cfg)
 
-    trainer.train_loader = torch.utils.data.DataLoader(
-        trainer.trainset, 
-        batch_size=cfg['batch_size'], 
-        shuffle=True,  
-        num_workers=cfg['workers'],
-        pin_memory=True) 
+#     trainer.train_loader = torch.utils.data.DataLoader(
+#         trainer.trainset, 
+#         batch_size=cfg['batch_size'], 
+#         shuffle=True,  
+#         num_workers=cfg['workers'],
+#         pin_memory=True) 
 
-    trainer.val_loader = torch.utils.data.DataLoader(
-        trainer.testset, 
-        batch_size=cfg['batch_size'], 
-        shuffle=False,  
-        num_workers=cfg['workers'],
-        pin_memory=True) 
+#     trainer.val_loader = torch.utils.data.DataLoader(
+#         trainer.testset, 
+#         batch_size=cfg['batch_size'], 
+#         shuffle=False,  
+#         num_workers=cfg['workers'],
+#         pin_memory=True) 
 
-    trainer.branch_init()
+#     trainer.branch_init()
 
-    try:
-        print("loading previous model...")
-        trainer.model.backbone.load_state_dict(
-            torch.load(trainer.cfg['model_path']), strict=False
-            )
-    except RuntimeError as e:
-        print(e)
-    except FileNotFoundError as e:
-        print(e)
+#     try:
+#         print("loading previous model...")
+#         trainer.model.backbone.load_state_dict(
+#             torch.load(trainer.cfg['model_path']), strict=False
+#             )
+#     except RuntimeError as e:
+#         print(e)
+#     except FileNotFoundError as e:
+#         print(e)
 
-    try:
-        for epoch in range(50):
-            trainer.branch_train(epoch)
-            trainer.scheduler.step()
-            trainer.branch_valid(epoch)
-    except KeyboardInterrupt:
-        print("terminating backbone training")
+#     try:
+#         for epoch in range(50):
+#             trainer.branch_train(epoch)
+#             trainer.scheduler.step()
+#             trainer.branch_valid(epoch)
+#     except KeyboardInterrupt:
+#         print("terminating backbone training")
 
-    try:
-        print("calibrating branch using temperature scale")
-        trainer.branch_calibrate()
+#     try:
+#         print("calibrating branch using temperature scale")
+#         trainer.branch_calibrate()
 
-    except KeyboardInterrupt:
-        print("terminating backbone training")
+#     except KeyboardInterrupt:
+#         print("terminating backbone training")
 
-    print("saving model to: ",cfg['model_path'])
-    torch.save(trainer.model.backbone.state_dict(), cfg['model_path'])
+#     print("saving model to: ",cfg['model_path'])
+#     torch.save(trainer.model.backbone.state_dict(), cfg['model_path'])
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
