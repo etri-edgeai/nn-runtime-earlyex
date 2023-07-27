9ba9225779ebed65ff1fc1320d56846fbe165f90
junyong300 <junyong300@gmail.com>
2023-07-27 17:06:50 +0900

    update

--- farmbot-anything/0.dataset_clean.py
+++ farmbot-anything/0.dataset_clean.py
@@ -1,21 +1,36 @@
+# Author: Junyong Park
 import pandas as pd
 import os
 import yaml
 
-def main(cfg):
-    df = pd.read_csv(cfg['0_input'])
-        
-    df = df[df['category'].notna()]
-    df = df[df['up'].notna()]
-    df = df[df['front'].notna()]
-    # Function to get the first word before comma
-    def get_first_word(text):
+# Function to get the first word before comma
+def get_first_word(text):
         if isinstance(text, str):
             return text.split(",")[0]
         else:
             return text
-        
+
+# Main function
+def main(cfg):
+    '''
+    This function reads the csv file and cleans the data.
+    args:
+        cfg: config file
+
+    returns:
+        None
+    '''
+    # Read the csv file
+    df = pd.read_csv(cfg['0_input'])
+    
+    # Drop the columns that are not available
+    df = df[df['category'].notna()]
     df['category'] = df['category'].apply(get_first_word)
+    
+    # Drop up and front columns that are not available
+    df = df[df['up'].notna()]
+    df = df[df['front'].notna()]
+        
     # Replace specific strings in 'category' with first word from 'wnlemmas'
     strings_to_replace = [
         '_StanfordSceneDBModels', 
@@ -28,6 +43,7 @@ def main(cfg):
         '_EvalSetInScenes', 
         '_EvalSetNoScenesNoPrior']
 
+    # Replace specific strings in 'category' with first word from 'wnlemmas'
     for string in strings_to_replace:
         df['category'] = df.apply(
             lambda row: get_first_word(row['wnlemmas']) \
@@ -36,28 +52,32 @@ def main(cfg):
     df['category'] = df['category'].str.title().str.replace(' ', '')
     df = df.loc[df['name'] != 'nan']
 
-    # Function to round the float values in a vector
+    # Function to round the vector
     def round_vector(vector):
-        # Split the string into components
         components = vector.split('\\,')
-        # Convert the components to floats, 
-        # round them, and convert them back to strings
         components = [str(round(float(component))) for component in components]
-        # Join the components back together
         return '\\,'.join(components)
 
     # Apply the function to the 'up' column
     df['up'] = df['up'].apply(round_vector)
     df['front'] = df['front'].apply(round_vector)
+
+    # Drop the columns that are not available
     df = df[df['unit'].notna()]
-    df2 = df[df['weight'].notna()]
-    df3 = df2[df2['surfaceVolume'].notna()]
-    df3 = df3[df3['category'].notna()]
-    df4 = df3.drop(columns=['wnsynset', 'wnlemmas', 'name', 'tags'])
-    df4 = df4.reset_index()
-    df4 = df4.rename(columns={'index': 'old_index'})
-    df4['category_id'] = pd.factorize(df4['category'])[0]
-    df4.to_csv(cfg['0_output'])
+    df = df[df['weight'].notna()]
+    df = df[df['surfaceVolume'].notna()]
+    df = df[df['category'].notna()]
+    df = df.drop(columns=['wnsynset', 'wnlemmas', 'name', 'tags'])
+    
+    # Reset the index and rename the column
+    df = df.reset_index()
+    df = df.rename(columns={'index': 'old_index'})
+    
+    # Add a new column with the new index
+    df['category_id'] = pd.factorize(df['category'])[0]
+
+    # Save the dataframe to a csv file
+    df.to_csv(cfg['0_output'])
 
 if __name__ == '__main__':
     with open("./config/config.yml", 'r') as f:
